3b8bd5205e73873d662d29433f3b3f061e170389
lalit058 <lalitnegi2058@gmail.com>
2026-08-23 23:14:40 +0545

    Add temporary superuser script

--- student_management_app/apps.py
+++ student_management_app/apps.py
@@ -1,5 +1,24 @@
-from django.apps import AppConfig
+import os
+from django.apps import AppConfig  # pyright: ignore[reportMissingImports]
+from django.contrib.auth import get_user_model  # pyright: ignore[reportMissingImports]
 
 
 class StudentManagementAppConfig(AppConfig):
-    name = 'student_management_app'
+  default_auto_field = "django.db.models.BigAutoField"
+  name = "student_management_app"
+
+  def ready(self):
+    if os.environ.get("RUN_MAIN") or os.getenv("RENDER"):
+      try:
+        User = get_user_model()
+        username = "superadmin"
+        email = "admin@example.com"
+        password = "Password123"
+
+        if not User.objects.filter(username=username).exists():
+          User.objects.create_superuser(
+              username=username, email=email, password=password
+          )
+          print(f"--> Superuser '{username}' was successfully created!")
+      except Exception:
+        pass
